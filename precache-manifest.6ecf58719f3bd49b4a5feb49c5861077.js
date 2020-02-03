b0cc6b2b63c845a104939ce64e59589a05a61cc1
Can Berk Ocalir <can@ocalir.dev>
2020-02-03 13:53:12 +0300

    Updates

---
+++ precache-manifest.6ecf58719f3bd49b4a5feb49c5861077.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390673916214c29c87fa963dde243c84",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a821387a5183d93e7694",
+    "url": "/monsters-rolodex/static/css/main.4343ddcf.chunk.css"
+  },
+  {
+    "revision": "364c6a7741c1458d55ea",
+    "url": "/monsters-rolodex/static/js/2.12430192.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.12430192.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a821387a5183d93e7694",
+    "url": "/monsters-rolodex/static/js/main.cad3f781.chunk.js"
+  },
+  {
+    "revision": "63210eb41ae177b68e9a",
+    "url": "/monsters-rolodex/static/js/runtime-main.70e0f7a3.js"
+  }
+]);
